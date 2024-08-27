5930d94909777e402ab471cdbd6de34a87052378
Hideto1008 <hidekiti1009@gmail.com>
2024-08-27 16:12:22 +0900

    Add users_model

---
+++ fp_app/spec/models/user_spec.rb
@@ -0,0 +1,44 @@
+# spec/models/user_spec.rb
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # ファクトリが有効かどうかのテスト
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
+
+  # バリデーションのテスト
+  it "is valid with a valid email, password, name, and icon_path" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = build(:user, email: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid without a password" do
+    user = build(:user, password: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email" do
+    create(:user, email: "duplicate@example.com")
+    user = build(:user, email: "duplicate@example.com")
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "has a default name of 'user'" do
+    user = create(:user)
+    expect(user.name).to eq("user")
+  end
+
+  it "has a default icon_path of an empty string" do
+    user = create(:user)
+    expect(user.icon_path).to eq("")
+  end
+end
