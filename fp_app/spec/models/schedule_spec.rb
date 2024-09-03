4bf1373e1d8534e274c4cd71d4dadca3a1381e5a
Hideto1008 <hidekiti1009@gmail.com>
2024-09-03 09:48:40 +0900

    Add valid rspec test

--- fp_app/spec/models/schedule_spec.rb
+++ fp_app/spec/models/schedule_spec.rb
@@ -22,33 +22,45 @@ RSpec.describe Schedule, type: :model do
     end
 
     it 'is invalid with a started_at on Sunday' do
-      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-08-04 10:00:00"), is_available: true) # 2024年8月4日は日曜日
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-29 10:00:00"), is_available: true) # 2024年9月29日は日曜日
       schedule.check_started_at
       expect(schedule.errors[:started_at]).to include("can't be on a Sunday")
     end
 
     it 'is invalid with a started_at before 11:00 on Saturday' do
-      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-08-03 10:30:00"), is_available: true) # 2024年8月3日は土曜日
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-28 10:30:00"), is_available: true) # 2024年9月28日は土曜日
       schedule.check_started_at
       expect(schedule.errors[:started_at]).to include("can't be before 11:00 on Saturday")
     end
 
     it 'is invalid with a started_at after 15:00 on Saturday' do
-      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-08-03 15:30:00"), is_available: true) # 2024年8月3日は土曜日
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-28 15:30:00"), is_available: true) # 2024年9月28日は土曜日
       schedule.check_started_at
       expect(schedule.errors[:started_at]).to include("can't be after 15:00 on Saturday")
     end
 
     it 'is invalid with a started_at before 10:00 on a weekday' do
-      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-08-01 09:00:00"), is_available: true) # 2024年8月1日は木曜日
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-30 09:00:00"), is_available: true) # 2024年9月30日は月曜日
       schedule.check_started_at
       expect(schedule.errors[:started_at]).to include("can't be before 10:00 on weekdays")
     end
 
     it 'is invalid with a started_at after 18:00 on a weekday' do
-      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-08-01 18:30:00"), is_available: true) # 2024年8月1日は木曜日
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-30 18:30:00"), is_available: true) # 2024年8月1日は木曜日
       schedule.check_started_at
       expect(schedule.errors[:started_at]).to include("can't be after 18:00 on weekdays")
+    end
+
+    it 'is valid with a started_at between 10:00 and 18:00 on a weekday' do
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-30 12:00:00"), is_available: true) # 2024年8月1日は木曜日
+      schedule.check_started_at
+      expect(schedule).to be_valid
+    end
+
+    it 'is valid with a started_at between 11:00 and 15:00 on Saturday' do
+      schedule = build(:schedule, planner: planner, started_at: Time.parse("2024-09-28 12:00:00"), is_available: true) # 2024年9月28日は土曜日
+      schedule.check_started_at
+      expect(schedule).to be_valid
     end
   end
 end
