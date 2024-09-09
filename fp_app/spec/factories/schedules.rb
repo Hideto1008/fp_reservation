dc4c22e1b893e559b5c73e8603370393f750eb75
Hideto1008 <hidekiti1009@gmail.com>
2024-09-09 14:44:53 +0900

    Modify schedule factory

--- fp_app/spec/factories/schedules.rb
+++ fp_app/spec/factories/schedules.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :schedule do
       planner
-      started_at { Time.parse("2024-09-30 17:00:00") }  
+      started_at { Time.current+1.day }  
       is_available { false }
     end
   end
