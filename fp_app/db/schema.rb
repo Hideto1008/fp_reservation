153251e37b9536b3ae1893b86977b96dc0aa1b03
Hideto1008 <hidekiti1009@gmail.com>
2024-09-09 12:04:01 +0900

    Modify planner_spec

--- fp_app/db/schema.rb
+++ fp_app/db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_26_080515) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_27_091457) do
+  create_table "planners", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_planners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_planners_on_reset_password_token", unique: true
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "user"
+    t.text "icon_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- fp_app/spec/models/planner_spec.rb
+++ fp_app/spec/models/planner_spec.rb
@@ -2,33 +2,36 @@
 require 'rails_helper'
 
 RSpec.describe Planner, type: :model do
-  # ファクトリが有効かどうかのテスト
-  it "has a valid factory" do
-    expect(build(:planner)).to be_valid
-  end
+  describe 'test for devise validation' do 
+    EMAIL = "duplicate@example.com"
+    # ファクトリが有効かどうかのテスト
+    it "has a valid factory" do
+      expect(build(:planner)).to be_valid
+    end
 
-  # バリデーションのテスト
-  it "is valid with a valid email, password" do
-    planner = build(:planner)
-    expect(planner).to be_valid
-  end
+    # バリデーションのテスト
+    it "is valid with a valid email, password" do
+      planner = build(:planner)
+      expect(planner).to be_valid
+    end
 
-  it "is invalid without an email" do
-    planner = build(:planner, email: nil)
-    expect(planner).not_to be_valid
-    expect(planner.errors[:email]).to include("can't be blank")
-  end
+    it "is invalid without an email" do
+      planner = build(:planner, email: nil)
+      expect(planner).not_to be_valid
+      expect(planner.errors[:email]).to include("can't be blank")
+    end
 
-  it "is invalid without a password" do
-    planner = build(:planner, password: nil)
-    expect(planner).not_to be_valid
-    expect(planner.errors[:password]).to include("can't be blank")
-  end
+    it "is invalid without a password" do
+      planner = build(:planner, password: nil)
+      expect(planner).not_to be_valid
+      expect(planner.errors[:password]).to include("can't be blank")
+    end
 
-  it "is invalid with a duplicate email" do
-    create(:planner, email: "duplicate@example.com")
-    planner = build(:planner, email: "duplicate@example.com")
-    expect(planner).not_to be_valid
-    expect(planner.errors[:email]).to include("has already been taken")
+    it "is invalid with a duplicate email" do
+      create(:planner, email: EMAIL)
+      planner = build(:planner, email: EMAIL)
+      expect(planner).not_to be_valid
+      expect(planner.errors[:email]).to include("has already been taken")
+    end
   end
 end
