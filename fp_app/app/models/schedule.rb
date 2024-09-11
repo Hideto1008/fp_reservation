e74cd344ea2583eba510ec25b423a1279aaf9cf5
Hideto1008 <hidekiti1009@gmail.com>
2024-09-11 11:14:54 +0900

    Add rubocop

--- fp_app/app/models/schedule.rb
+++ fp_app/app/models/schedule.rb
@@ -12,36 +12,36 @@ class Schedule < ApplicationRecord
   end
 
   def check_schedule_within_working_hours
-		check_not_closed_day
-  	check_valid_started_at_in_saturday
-		check_valid_started_at_in_weekdays
-	end		
+    check_not_closed_day
+    check_valid_started_at_in_saturday
+    check_valid_started_at_in_weekdays
+  end
 
-	private
-	def check_not_closed_day
-		return unless started_at.sunday?
-		if is_available
+  private
+  def check_not_closed_day
+    return unless started_at.sunday?
+    if is_available
       errors.add(:started_at, "can't be on a closed day")
-		end
-	end
+    end
+  end
 
-	def check_valid_started_at_in_saturday
-		return unless started_at.saturday?
-		if is_available && started_at < started_at.change(hour: 11, min: 0)
-			errors.add(:started_at, "can't be before 11:00 on Saturday")
-		end
-		if is_available && started_at >= started_at.change(hour: 15, min: 0)
-			errors.add(:started_at, "can't be after 15:00 on Saturday")
-		end
-	end
+  def check_valid_started_at_in_saturday
+    return unless started_at.saturday?
+    if is_available && started_at < started_at.change(hour: 11, min: 0)
+      errors.add(:started_at, "can't be before 11:00 on Saturday")
+    end
+    if is_available && started_at >= started_at.change(hour: 15, min: 0)
+      errors.add(:started_at, "can't be after 15:00 on Saturday")
+    end
+  end
 
-	def check_valid_started_at_in_weekdays
-		return if started_at.saturday? || started_at.sunday?
-		if started_at < started_at.change(hour: 10, min: 0) && is_available
-			errors.add(:started_at, "can't be before 10:00 on weekdays")
-		end
-		if started_at >= started_at.change(hour: 18, min: 0) && is_available
-			errors.add(:started_at, "can't be after 18:00 on weekdays")
-		end
-	end
+  def check_valid_started_at_in_weekdays
+    return if started_at.saturday? || started_at.sunday?
+    if started_at < started_at.change(hour: 10, min: 0) && is_available
+      errors.add(:started_at, "can't be before 10:00 on weekdays")
+    end
+    if started_at >= started_at.change(hour: 18, min: 0) && is_available
+      errors.add(:started_at, "can't be after 18:00 on weekdays")
+    end
+  end
 end
