0a08800d27dbc7602b4eb1fe40e0404bebb793a8
Hideto1008 <hidekiti1009@gmail.com>
2024-09-11 18:19:22 +0900

    Modify routing

--- fp_app/config/routes.rb
+++ fp_app/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'hello', to: 'hello_world#index'
   get 'planners/:id/mypage', to: 'planners/main#mypage', as: 'planners_mypage'
   get 'planners/:id/schedule', to: 'planners/main#schedule'
+  get 'planners/:id/edit_planner_info', to: 'planners/main#edit_planner_info', as: 'edit_planner_info'
+  patch 'planners/:id/update_info', to: 'planners/main#update_info', as: 'planner_update_info'
+
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
