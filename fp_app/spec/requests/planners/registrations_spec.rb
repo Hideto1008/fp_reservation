23cbaf8b5014d9e950c0c8f6f71546c8ec83ce1f
Hideto1008 <hidekiti1009@gmail.com>
2024-09-11 14:12:08 +0900

    Create home page

---
+++ fp_app/spec/requests/planners/registrations_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Planners::RegistrationsController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  EMAIL = "test_planner@example.com"
+  before do
+    # Deviseのマッピングを設定
+    @request.env["devise.mapping"] = Devise.mappings[:planner]
+  end
+
+  describe 'POST #create' do
+    it 'redirects to /hello after planner sign up' do
+      planner_params = {
+        planner: {
+          email: EMAIL,
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+      post :create, params: planner_params
+      expect(response).to redirect_to('/hello')
+    end
+  end
+end
