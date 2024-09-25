38b70c868fba116a42e57fb281dc0cf69593a4c3
Hideto1008 <hidekiti1009@gmail.com>
2024-09-25 15:16:14 +0900

    Modify planner mypage

--- fp_app/config/routes.rb
+++ fp_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "homepages#index"
   get "hello", to: "hello_world#index"
   get "planners/:id/mypage", to: "planners/main#mypage", as: "planners_mypage"
-  get "planners/:id/schedule", to: "planners/main#schedule"
+  get "planners/:id/schedule", to: "planners/main#schedule", as: "planners_schedule"
   get "planners/:id/edit_planner_info", to: "planners/main#edit_planner_info", as: "edit_planner_info"
   patch "planners/:id/update_info", to: "planners/main#update_planner_info", as: "update_planner_info"
 
--- fp_app/spec/factories/planners.rb
+++ fp_app/spec/factories/planners.rb
@@ -3,5 +3,17 @@ FactoryBot.define do
     email { "test@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    name { "Test Planner" }
+    icon_path { "/path/to/icon.png" }
+    introduction { "This is a test planner." }
+  end
+
+  factory :other_planner, class: Planner do
+    email {"other@example.com"}
+    password {"password"}
+    password_confirmation {"password"}
+    name {"Other Planner"}
+    icon_path {"/path/to/other_icon.png"}
+    introduction {"This is another planner."}
   end
 end
