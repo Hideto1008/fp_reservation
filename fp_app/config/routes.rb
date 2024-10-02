ac709058881302693b85525c3d3c6e146b6bca12
Hideto1008 <hidekiti1009@gmail.com>
2024-10-02 16:51:13 +0900

    Add user mypage

--- fp_app/config/routes.rb
+++ fp_app/config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   root "homepages#index"
   get "hello", to: "hello_world#index"
+
   get "planners/:id/mypage", to: "planners/mypages#mypage", as: "planners_mypage"
   get "planners/:id/edit_planner_info", to: "planners/mypages#edit_planner_info", as: "edit_planner_info"
   patch "planners/:id/update_info", to: "planners/mypages#update_planner_info", as: "update_planner_info"
+
   get "planners/:id/schedule", to: "planners/schedules#schedule", as: "planners_schedule"
   post "planners/:id/schedules", to: "planners/schedules#create_schedule", as: "create_schedule"
   patch "planners/:id/schedules/:schedule_id/update_schedule", to: "planners/schedules#update_schedule", as: "update_schedule"
+
+  get "users/:id/mypage", to: "users/mypages#mypage", as: "users_mypage"
+  get "users/:id/edit_user_info", to: "users/mypages#edit_user_info", as: "edit_user_info"
+  patch "users/:id/update_info", to: "users/mypages#update_user_info", as: "update_user_info"
+
+  get "users/:id/reservation", to: "users/appointments#index", as: "reservation"
+  get "users/planner_list", to: "users/planners#index", as: "planners_list"
 
   devise_for :users, controllers: {
     sessions: "users/sessions",
--- fp_app/db/seeds.rb
+++ fp_app/db/seeds.rb
@@ -6,22 +6,28 @@ planner = Planner.create!(
   introduction: "はじめまして、フィナンシャルプランナーのfp_ex1です。ライフプラン設計や資産運用、保険の見直しなど、個々のニーズに合わせたご提案を心掛けています。お客様の将来設計をサポートし、安心できる生活を実現するため、誠心誠意お手伝いいたします。どうぞお気軽にご相談ください。"
 )
 
+user = User.create!(
+  email: "user_sample@example.com",
+  password: "password",
+  name: "user_ex",
+  icon_path: "https://free-icon.org/material/02-illustration/0139-download-image-m.png"
+)
+
 schedule_times = [
-  { started_at: Time.new(2024, 10, 01, 10, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 01, 11, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 01, 13, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 01, 14, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 01, 15, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 02, 16, 0, 0), is_available: true },
-  { started_at: Time.new(2024, 10, 02, 17, 0, 0), is_available: true }
+  { started_at: (Time.now.beginning_of_week + 1.week) + 12.hours, is_available: true },
+  { started_at: (Time.now.beginning_of_week + 1.week) + 13.hours, is_available: true },
+  { started_at: (Time.now.beginning_of_week + 1.week) + 14.hours, is_available: true },
+  { started_at: (Time.now.beginning_of_week + 1.week) + 15.hours, is_available: true },
+  { started_at: (Time.now.beginning_of_week + 1.week) + 16.hours, is_available: true },
+  { started_at: (Time.now.beginning_of_week + 1.week) + 17.hours, is_available: true },
 ]
 
 schedule_times.each do |time|
   Schedule.create!(
-    planner_id: planner.id,
+    planner: planner,
     started_at: time[:started_at],
     is_available: time[:is_available]
   )
 end
 
-puts "Planner and schedules created successfully."
+puts "seed created successfully."
