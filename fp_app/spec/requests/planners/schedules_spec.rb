81fed177db6bda4625ef072391eed15c655f328d
Hideto1008 <hidekiti1009@gmail.com>
2024-10-16 09:54:01 +0900

    Modify schedules_spec

--- fp_app/spec/requests/planners/schedules_spec.rb
+++ fp_app/spec/requests/planners/schedules_spec.rb
@@ -40,8 +40,9 @@ RSpec.describe "Planners::Schedules", type: :request do
 
     context "when creating a schedule for another planner" do
       it "redirects to root_path" do
-        post planner_schedules_path(other_planner), params: valid_params, xhr: true
-        not_to change(Schedule, :count)
+        expect {
+          post planner_schedules_path(other_planner), params: valid_params, xhr: true
+        }.not_to change { Schedule.count }
         expect(response).to redirect_to(root_path)
       end
     end
