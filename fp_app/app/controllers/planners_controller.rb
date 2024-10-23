ce31fea83162bba90037adc89bc712bd260cd2b8
Hideto1008 <hidekiti1009@gmail.com>
2024-10-23 10:03:23 +0900

    Modify planner show

--- fp_app/app/controllers/planners_controller.rb
+++ fp_app/app/controllers/planners_controller.rb
@@ -1,7 +1,7 @@
 class PlannersController < ApplicationController
-  before_action :authenticate_planner!, only: %i[show edit update]
+  before_action :authenticate_planner!, only: %i[edit update]
   before_action :authenticate_user!, only: %i[index]
-  before_action :correct_planner, only: %i[show edit update]
+  before_action :correct_planner, only: %i[edit update]
 
 
   def index
