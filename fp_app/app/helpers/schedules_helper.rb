fe2cf3d1b2146ae6ec0acff58791362d68b1f66b
Hideto1008 <hidekiti1009@gmail.com>
2024-11-11 15:08:45 +0900

    Add association function

--- fp_app/app/helpers/schedules_helper.rb
+++ fp_app/app/helpers/schedules_helper.rb
@@ -17,7 +17,7 @@ module SchedulesHelper
       matching_schedule = schedules.find { |s| s.started_at.to_date == day && s.started_at.strftime("%H:%M") == time }
 
       if matching_schedule
-        is_reserved = Appointment.exists?(schedule_id: matching_schedule.id, status: "reserved")
+        is_reserved = matching_schedule.appointment.exists?(status: "reserved")
         link_to(is_reserved ? "&#x2612;".html_safe: (matching_schedule.is_available ? "&#9711;".html_safe: "&#10005;".html_safe), planner_schedule_path(planner_id: @planner.id, id: matching_schedule.id, is_available: !matching_schedule.is_available),
           method: :patch, remote: true, class: "toggle-availability",
           data: { schedule_id: matching_schedule.id },
