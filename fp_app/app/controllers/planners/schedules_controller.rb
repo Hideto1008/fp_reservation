9bf394992161589b0859a14dedeb5427ed9972a3
Hideto1008 <hidekiti1009@gmail.com>
2024-11-15 10:04:42 +0900

    Detach index function to private method

--- fp_app/app/controllers/planners/schedules_controller.rb
+++ fp_app/app/controllers/planners/schedules_controller.rb
@@ -10,10 +10,8 @@ class Planners::SchedulesController < ApplicationController
 
     if user_signed_in?
       @planners = Planner.page(params[:page]).per(ITEMS_PER_PAGE)
-
-      unless request.referer&.match(%r{/planners/#{@selected_planner.id}/schedules(\?.*)?$})
-        planner_position = Planner.order(:id).pluck(:id).index(@selected_planner.id)
-        page_number = (planner_position / ITEMS_PER_PAGE) + 1
+      unless came_from_planners_schedules_page?
+        page_number = calculate_page_number(@selected_planner)
         if page_number != params[:page].to_i
           redirect_to planner_schedules_path(planner_id: @selected_planner.id, page: page_number)
         end
@@ -79,5 +77,14 @@ class Planners::SchedulesController < ApplicationController
 
   def planner_params
     params.require(:planner).permit(:name, :icon_path, :introduction)
+  end
+
+  def came_from_planners_schedules_page?
+    request.referer&.match(%r{/planners/#{@selected_planner.id}/schedules(\?.*)?$})
+  end
+
+  def calculate_page_number(selected_planner)
+    planner_position = Planner.order(:id).pluck(:id).index(selected_planner.id)
+    (planner_position / ITEMS_PER_PAGE) + 1
   end
 end
