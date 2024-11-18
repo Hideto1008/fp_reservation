2dd044b3b2ad709e1a5a8585ea93bd296d48a1ab
Hideto1008 <hidekiti1009@gmail.com>
2024-11-18 17:23:34 +0900

    Render homepage index

--- fp_app/app/controllers/homepages_controller.rb
+++ fp_app/app/controllers/homepages_controller.rb
@@ -1,5 +1,5 @@
 class HomepagesController < ApplicationController
   def index
-    render html: "Welcome to the Find a Planner App!"
+    # render html: "Welcome to the Find a Planner App!"
   end
 end
