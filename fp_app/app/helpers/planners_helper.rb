c9ede513cfd3c0c1549180f7dfcc0a2050c380b3
Hideto1008 <hidekiti1009@gmail.com>
2024-11-20 10:44:46 +0900

    Run rubocop

--- fp_app/app/helpers/planners_helper.rb
+++ fp_app/app/helpers/planners_helper.rb
@@ -1,8 +1,8 @@
 module PlannersHelper
   def sort_option(name_cont)
     [
-      ['Order of registration', planners_path(sort: 'order_of_registration', name_cont: name_cont)],
-      ['Total of Consultations', planners_path(sort: 'total_of_consultations', name_cont: name_cont)]
+      [ "Order of registration", planners_path(sort: "order_of_registration", name_cont: name_cont) ],
+      [ "Total of Consultations", planners_path(sort: "total_of_consultations", name_cont: name_cont) ]
     ]
   end
 end
