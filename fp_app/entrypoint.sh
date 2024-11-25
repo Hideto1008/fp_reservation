bd50a9fe4611cf65b8fb60633b8a2cc1f41445c7
Hideto1008 <hidekiti1009@gmail.com>
2024-11-25 12:33:00 +0900

    Change app ports number

--- fp_app/entrypoint.sh
+++ fp_app/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f "$PWD/tmp/pids/server.pid"
 
+# Use PORT environment variable or default to 3000
+PORT=${PORT:-3000}
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec bundle exec rails server -b 0.0.0.0 -p $PORT
