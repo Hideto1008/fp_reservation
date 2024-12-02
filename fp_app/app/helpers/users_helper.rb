04c813eb698272627667c433cac235020449e62c
Hideto1008 <hidekiti1009@gmail.com>
2024-12-02 11:44:59 +0900

    Define set__icon method in helper

--- fp_app/app/helpers/users_helper.rb
+++ fp_app/app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def set_user_icon(user)
-    user.icon_path.presence || asset_path('user_default_icon.png')
+    user.icon_path.presence || asset_path('default_icon.png')
   end
 end
