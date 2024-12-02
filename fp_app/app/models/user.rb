41ef19bb783cb8efa2882cdcba4df19db3ccbf1f
Hideto1008 <hidekiti1009@gmail.com>
2024-12-02 11:02:29 +0900

    Define a validate that sets the default icon for the model

--- fp_app/app/models/user.rb
+++ fp_app/app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :appointments, dependent: :destroy
+  before_validation :set_default_icon_path, on: [:create, :update]
+
+  def set_default_icon_path
+    self.icon_path = ActionController::Base.helpers.asset_path("user_default_icon.png") if self.icon_path.blank?
+  end
 end
